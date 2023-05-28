dfda17743043a575927b56b6a4bf2fa108f572ce
Zeno Romio <79644242+zenoromio@users.noreply.github.com>
2023-05-28 21:41:17 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,34 +3,43 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import links
 
+#this whole block of code will open the url with selenium and extract the html with BeautifulSoup and store it inside the "soup" variable
 chrome_options = Options()  
 chrome_options.add_argument("--headless")
 chrome_options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(options=chrome_options)
-driver.get(links.link4)
+driver.get(links.link11)
 soup = BeautifulSoup(driver.page_source, "html.parser")
 driver.close()
 
+#these are the class names found inside the nike.com website
 shoes_name = "mt2-sm css-12whm6j"
-hoodies_name = "mt2-sm css-hzulvp"
+hoodies_man = "mt2-sm css-hzulvp"
+hoodies_woman ="mt2-sm css-1j3x2vp"
 
-
-sizes = soup.find_all("div", class_=shoes_name)
+#find all element with class name (class name must match with selected link)
+sizes = soup.find("div", class_=hoodies_woman)
 
 
 def find_sizes():
-    for size in sizes:
-        divs = size.find_all("div")
-        for input in divs:
-            if "disabled" not in str(input):
-                print(input.text) 
+    #get each size
+    size_array = sizes.find_all("div")
+
+    #print out each available size
+    for size in size_array:
+        #the unavailable shoes are marked with "disabled" in html file
+        if "disabled" not in str(size):
+            print(size.text) 
+
 
 def find_available_sizes():
-    for size in sizes:
-        divs = size.find_all("div")
-        for input in divs:
-            print(input.text) 
+    #get each size
+    size_array = sizes.find_all("div")
+
+    #print out each available size
+    for size in size_array:
+        print(size.text) 
+
 
 find_sizes()
-print("")
 find_available_sizes()
--- selenium_test.py
+++ selenium_test.py
@@ -3,19 +3,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import links
 
-chrome_options = Options()  
+chrome_options = Options()
 chrome_options.add_argument("--headless")  
 chrome_options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(options=chrome_options)
 
+#these are the class names found inside the nike.com website
 shoes_name = "mt2-sm.css-12whm6j"
-hoodies_name = "mt2-sm.css-hzulvp"
+hoodies_man = "mt2-sm.css-hzulvp"
+hoodies_woman ="mt2-sm.css-1j3x2vp"
 
 
 def find_sizes(url, class_name):
     driver.get(url)
+    #from url get the part with all the sizes
     sizes = driver.find_element(By.CLASS_NAME, class_name)
+    #from all the sizes create an array with each size
     size_array = sizes.find_elements(By.TAG_NAME, "div")
+
+    #print out each size
     for i in size_array:
         label = i.find_element(By.TAG_NAME, "label")
         inputs = i.find_element(By.TAG_NAME, "input")
@@ -24,18 +30,23 @@ def find_sizes(url, class_name):
 
 def find_available_sizes(url, class_name):
     driver.get(url)
+    #from url get the part with all the sizes
     sizes = driver.find_element(By.CLASS_NAME, class_name)
+    #from all the sizes create an array with each size
     size_array = sizes.find_elements(By.TAG_NAME, "div")
+
+    #print out only available shoes
     for i in size_array:
         label = i.find_element(By.TAG_NAME, "label")
         inputs = i.find_element(By.TAG_NAME, "input")
 
+        #the unavailable shoes are marked with "disabled" in html file
         if not inputs.get_attribute("disabled"):
             print(label.get_attribute("innerHTML"))
 
 
+#when calling the function you have to pass the url and the class name as parameters
+find_available_sizes(links.link9, hoodies_woman)
+find_sizes(links.link9, hoodies_woman)
 
-find_available_sizes(links.link4, shoes_name)
-print("")
-find_sizes(links.link4, shoes_name)
 driver.close()
